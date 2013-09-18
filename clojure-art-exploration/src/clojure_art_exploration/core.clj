259a311554d303609ffa5f0798d5dbde9dc6f459
Max Magnuson <magnu401@tedlar.morris.umn.edu>
2013-09-18 15:35:31 -0500

    Initial commit of the practice project

---
+++ clojure-art-exploration/src/clojure_art_exploration/core.clj
@@ -0,0 +1,1 @@
+(ns clojure-art-exploration.core)
---
+++ clojure-art-exploration/test/clojure_art_exploration/test/core.clj
@@ -0,0 +1,6 @@
+(ns clojure-art-exploration.test.core
+  (:use [clojure-art-exploration.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
