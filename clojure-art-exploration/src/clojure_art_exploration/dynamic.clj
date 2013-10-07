d30b43634cfcbd647416bc0b9f1b245d69f79ace
Paul Schliep <schli202@tang.morris.umn.edu>
2013-10-06 20:39:45 -0500

    Added flames and a moon, better animation and changed the siezure a bit

--- clojure-art-exploration/src/clojure_art_exploration/dynamic.clj
+++ clojure-art-exploration/src/clojure_art_exploration/dynamic.clj
@@ -14,32 +14,65 @@
 (stroke-weight 5)
 (stroke 220 20 60)
 (let [flameXpos 880
-      flameYpos (- (screen-height) (* (frame-count) 20))]
+      flameYpos (- (screen-height) (* (frame-count) 20))
+      pi (/ 22 7)]
+   
+   ;; Thrusters
+   (fill 128 128 128)
+   (stroke 128 128 128)
+   (arc (+ 5 flameXpos) (+ flameYpos 15) 25 25 pi (* 2 pi))
+   (arc (+ 40 flameXpos) (+ flameYpos 15) 25 25 pi (* 2 pi))
+   (arc (+ 75 flameXpos) (+ flameYpos 15) 25 25 pi (* 2 pi))
+
    (if (= (mod (frame-count) 2) 0)	
-   	(do (begin-shape :triangles)
-		(triangle flameXpos flameYpos (- flameXpos 20) (+ (+ flameYpos 30) (* (frame-count) 2) ) (- flameXpos 10) (+ (+ flameYpos 10) (* (frame-count) 2) ) )
-		(triangle (+ flameXpos 10) flameYpos (- flameXpos 10) (+ (+ flameYpos 10) (* (frame-count) 2) ) (- flameXpos 10) (+ (+ flameYpos 45) (* (frame-count) 2) ))
-        	(triangle (+ flameXpos 20) flameYpos (+ flameXpos 20) (+ (+ flameYpos 45) (* (frame-count) 2) ) (- flameXpos 66) (+ (+ flameYpos 80) (* (frame-count) 2) ))
-        	(triangle (+ flameXpos 60) flameYpos (+ flameXpos 60) (+ (+ flameYpos 45)(* (frame-count) 2) ) (+ flameXpos 66) (+ (+ flameYpos 80) (* (frame-count) 2) ))
-		(triangle (+ flameXpos 70) flameYpos (+ flameXpos 80) (+ (+ flameYpos 10) (* (frame-count) 2) ) (+ flameXpos 20) (+ (+ flameYpos 90) (* (frame-count) 2) ))
-        	(triangle (+ flameXpos 80) flameYpos (+ flameXpos 100) (+ (+ flameYpos 30) (* (frame-count) 2) ) (+ flameXpos 120) (+ (+ flameYpos 30) (* (frame-count) 2) ))        
+   	(do (fill 255 127 0)
+        (stroke 220 20 60)
+	(begin-shape :triangles)
+		(triangle (- flameXpos 7.5) (+ flameYpos 15) (+ flameXpos 5) (+ flameYpos 50) (+ flameXpos 17.5)  (+ flameYpos 15) ) 
+                (triangle (+ flameXpos 27.5) (+ flameYpos 15) (+ flameXpos 40) (+ flameYpos 50) (+ flameXpos 52.5)  (+ flameYpos 15) ) 
+        	(triangle (+ flameXpos 62.5) (+ flameYpos 15) (+ flameXpos 75) (+ flameYpos 50) (+ flameXpos 87.5)  (+ flameYpos 15) ) ) 
+
    	(end-shape))
-   	(do (begin-shape :lines)
-		(line flameXpos flameYpos (- flameXpos 20) (+ (+ flameYpos 15) (* (frame-count) 2) ) )
-		(line (+ flameXpos 10) flameYpos (- flameXpos 10) (+ (+ flameYpos 5) (* (frame-count) 2) ) )
-        	(line (+ flameXpos 20) flameYpos (+ flameXpos 20) (+ (+ flameYpos 25) (* (frame-count) 2) ) )
-        	(line (+ flameXpos 60) flameYpos (+ flameXpos 60) (+ (+ flameYpos 25)(* (frame-count) 2) ) )
-		(line (+ flameXpos 70) flameYpos (+ flameXpos 80) (+ (+ flameYpos 5) (* (frame-count) 2) ) )
-        	(line (+ flameXpos 80) flameYpos (+ flameXpos 100) (+ (+ flameYpos 15) (* (frame-count) 2) ) )        
-   	(end-shape))
-   ))
+   )
+)	
+
+(defn drawMoon []
+  (fill 220 220 220)
+  (stroke-weight 5)
+  (stroke 0 0 0)
+  (arc 1500 200 200 200 0 6.283)
+  (ellipse (+ (random 75) 1500) (+ (random 75) 200) 3 3)
+  (ellipse (+ (random 75) 1500) (+ (random 75) 200) 3 3)
+  (ellipse (+ (random 75) 1500) (+ (random 75) 200) 3 3)
+  (ellipse (+ (random 75) 1500) (+ (random 75) 200) 3 3)
+  (ellipse (+ (random 75) 1500) (+ (random 75) 200) 3 3)
+
+  (ellipse (+ (random 75) 1500) (- 200 (random 75)) 3 3)
+  (ellipse (+ (random 75) 1500) (- 200 (random 75)) 3 3)
+  (ellipse (+ (random 75) 1500) (- 200 (random 75)) 3 3)
+  (ellipse (+ (random 75) 1500) (- 200 (random 75)) 3 3)
+  (ellipse (+ (random 75) 1500) (- 200 (random 75)) 3 3)
+
+  (ellipse (- 1500 (random 75)) (+ (random 75) 200) 3 3)
+  (ellipse (- 1500 (random 75)) (+ (random 75) 200) 3 3)
+  (ellipse (- 1500 (random 75)) (+ (random 75) 200) 3 3)
+  (ellipse (- 1500 (random 75)) (+ (random 75) 200) 3 3)
+  (ellipse (- 1500 (random 75)) (+ (random 75) 200) 3 3)
+
+  (ellipse (- 1500 (random 75)) (- 200 (random 75)) 3 3)
+  (ellipse (- 1500 (random 75)) (- 200 (random 75)) 3 3)
+  (ellipse (- 1500 (random 75)) (- 200 (random 75)) 3 3)
+  (ellipse (- 1500 (random 75)) (- 200 (random 75)) 3 3)
+  (ellipse (- 1500 (random 75)) (- 200 (random 75)) 3 3)
+  ;;(do (doall (take 20 (repeat (ellipse (+ (random 80) 1500) (+ (random 80) 200) 5 5))))
+  ;;(doall (take 20 (repeat (ellipse (- (random 80) 1500) (- (random 80) 200) 5 5)))))	
 )
 
 (defn drawRocket []
   (frame-rate (frame-count))
-  (stroke 255 (* (frame-count) 7))
+  (stroke 255)
   (stroke-weight 10)
-  (fill 220 20 60 (* (frame-count) 7))
+  (fill 220 20 60)
   (let [widthBody 80 
 	heightBody 120
         rocketXposition 880
@@ -54,12 +87,15 @@
   (fill (random 255)(random 255)(random 255))
   (frame-rate 30) 
   (text-font (create-font "Utopia Italic" (random 250) true))
-  (if (= (mod (frame-count) 10) 0) (text "CLOJURE!!!" (random 800) (random 800)) (background (random 255)(random 255)(random 255)) )
+  (if (= (mod (frame-count) 10) 0)  
+	(text "CLOJURE!!!" (random 800) (random 800)) 
+	(do (background (random 255)(random 255)(random 255))(set-image (+ 850 (random 35)) (+ 400 (random 35)) (load-image "nick-cage-as-a-baby.jpg")) ) )
 )
 
 (defn draw []
   (if (< (frame-count) 60)
   	      (do (background 0 255 255 75) 
+              (drawMoon)
               (fill 255 0 0)
               (text-font (create-font "Liberation Serif Bold" 250 true))
               (text "America!" 100 600)
@@ -67,7 +103,7 @@
     
     (seizure)  
   )
-  (if (= (frame-count) 200) (System/exit 0))
+  (if (= (frame-count) 100) (System/exit 0))
 )
 
 
